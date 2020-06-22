2e6b1a4e6ceacd3493a37a2be91e3810f4607ec4
DefCon-018 <hp761570@gmil.com>
2020-06-22 17:37:28 +0530

    simple quiz project
    Signed-off-by: DefCon-018 <hp761570@gmil.com>

---
+++ quiz/src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+       <b-navbar toggleable="sm" type="light" variant="light" tabs>
+       <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+
+       <b-navbar-brand>Quiz Questions</b-navbar-brand>
+
+       <b-collapse id="nav-text-collapse" is-nav>
+      <b-navbar-nav>
+         <b-nav-text>Counter: {{numCorrect}}/{{numTotal}}</b-nav-text>
+         </b-navbar-nav>
+         </b-collapse>
+      </b-navbar>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [
+        'numCorrect',
+        'numTotal'
+    ]
+}
+</script>
